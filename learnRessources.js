232b41fd8d0edba85ce0418b77b35c57deb34d87
LACOSTAR91 <lacostar91@outlook.com>
2022-09-06 15:48:07 +0200

    Create learnRessources.js

---
+++ learnRessources.js
@@ -0,0 +1,141 @@
+/**
+ * L'object pour la liste des ressources pour apprendre un langage/framwork vérifier par DC.
+ * @type {Object}
+ */
+module.exports.Learn = {
+    "JavaScript": {
+        "Cours": [
+            "[JavaScript.com ressources](https://www.javascript.com/resources)",
+            "[Grafikart](https://grafikart.fr/formations/debuter-javascript)",
+            "[OpenClassrooms JavaScript](https://openclassrooms.com/fr/courses/6175841-apprenez-a-programmer-avec-javascript)",
+            "[OpenClassrooms JavaScript Web](https://openclassrooms.com/fr/courses/5543061-ecrivez-du-javascript-pour-le-web)",
+            "[Udemy JavaScript Cours (payant)](https://www.udemy.com/course/the-complete-javascript-course/)",
+            "[JavaScript for cats cours](http://jsforcats.com/)",
+            "[Bitdegree JavaScript basics](https://www.bitdegree.org/learn/javascript-basics)",
+            "[Adrienjoly basics JavaScript](http://adrienjoly.com/cours-javascript/)",
+            "[Khanacademy cours](https://fr.khanacademy.org/computing/computer-programming/html-css-js)",
+            
+        ],
+        "Documentation": [
+            "[MDN JavaScript](https://developer.mozilla.org/fr/docs/Web/JavaScript)",
+            "[MDN JavaScript Reference](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference)",
+            "[OverAPI](https://overapi.com/javascript)",
+            "[JavaScript.com documentation](https://www.javascript.com/learn)",
+            "[ExploringJS](https://exploringjs.com/impatient-js/ch_syntax.html#statement-vs.expression)",
+        ],
+        "Exercices": [
+            "[Coding Game](https://www.codingame.com/start)",
+            "[JavaScript Cours](https://www.odyssey.sdlm.be/)"
+        ],
+        "Vidéos": [
+            "[JavaScript - GetCodingKnowledge](https://www.youtube.com/watch?v=s_VMwHFSjXY&list=PLuWyq_EO5_AI83Z2JdSPdJ-81QPvI3cxC)",
+        ]
+    },
+    "HTML/CSS/SASS": {
+        "Documentation": [
+            "[MDN HTML Balises](https://developer.mozilla.org/fr/docs/Web/HTML/Element)",
+            "[MDN CSS](https://developer.mozilla.org/fr/docs/Web/CSS/)",
+            "[W3schools CSS](https://www.w3schools.com/cssref/)",
+        ],
+        "Exercices": [
+            "[CSS Battle](https://cssbattle.dev/)",
+            "[Flexbox Defense](http://www.flexboxdefense.com/)",
+            "[Devchallenges](https://devchallenges.io/)",
+            "[CSS Diner](https://flukeout.github.io/)",
+            "[Flexbox Zombies](https://flexboxzombies.com/p/flexbox-zombies)",
+            "[Flexbox Froggy](https://flexboxfroggy.com/#fr)",
+            "[Frontend Mentor](https://www.frontendmentor.io/challenges)"
+        ],
+        "Vidéos": [
+            "[HTML/CSS - Pierre Giraud](https://www.youtube.com/watch?v=Y80juYcu3ZI&list=PLwLsbqvBlImHG5yeUCXJ1aqNMgUKi1NK3)",
+            "[HTML - From Scratch Développement Web](https://www.youtube.com/watch?v=qsbkZ7gIKnc)",
+            "[CSS - From Scratch Développement Web](https://www.youtube.com/watch?v=HDobHQfbRbo)",
+            "[HTML - Grafikart](https://www.youtube.com/watch?v=oEAuNzWXRjM&list=PLjwdMgw5TTLUeixVGPNl1uZNeJy4UY6qX)",
+            "[CSS - Grafikart](https://www.youtube.com/watch?v=PE8FQ6zihhw&list=PLjwdMgw5TTLVjTZQocrMwKicV5wsZlRpj)",
+            "[HTML/CSS (Ancienne série) - Graven](https://www.youtube.com/watch?v=J9w-cir5a6U&list=PLMS9Cy4Enq5JAzNgWPK96HnkE_U7Ol3im)",
+            "[HTML/CSS (Nouvelle série) - Graven](https://www.youtube.com/watch?v=6hCGTJCo_Uo)",
+            "[HTML/CSS/SASS + framwork (Bulma, Tailwind CSS, Bootstrap) - GetCodingKnowledge](https://www.youtube.com/watch?v=gjkSRjEEwXE&list=PLuWyq_EO5_AKQWq5DdhqYNGcHfMTajfr3)",
+            "[SASS - Grafikart](https://www.youtube.com/watch?v=aOccUzHD_MQ&list=PLjwdMgw5TTLWVp8WUGheSrGnmEWIMk9H6)",
+        ]
+    },
+    "Node.JS": {
+        "Documentation": [
+            "[Node.JS Documentation](https://nodejs.org/fr/docs/)",
+        ],
+        "Vidéos": [
+            "[Node.JS pour débutants - GetCodingKnowledge](https://www.youtube.com/playlist?list=PLuWyq_EO5_AJmkNV0eFfjWR9QSdfGwzCx)",
+        ]
+    },
+    "Discord.js": {
+        "Documentation": [
+            "[Discord.js documentation](https://discord.js.org/)",
+            "[Discord.js guide](https://discordjs.guide/)",
+        ],
+        "Vidéos": [
+            "[Discord.js V13 pour débutants - GetCodingKnowledge](https://www.youtube.com/watch?v=UKanuYjWDMU&list=PLuWyq_EO5_AJ4pXBLXi3KgAb4_uYVkmr3)",
+            "[Discord.js V13 (playlist incomplète) - Dev-Community](https://www.youtube.com/watch?v=YitVkjM2Cec&list=PLpBtWRenhW_S0fRWfxZZYDQZLYuLI0NPX)"
+        ]
+    },
+    "Golang": {
+        "Documentation": [
+            "[Golang documentation](https://golang.org/doc/)",
+        ],
+        "Vidéos": [
+            "[Golang pour débutants - GetCodingKnowledge](https://www.youtube.com/watch?v=LyragMPx77c&list=PLuWyq_EO5_AKP_KCaIr53UfOqlPThTXat)",
+        ]
+    },
+    "PHP": {
+        "Documentation": [
+            "[PHP documentation](https://www.php.net/manual/fr/)",
+        ],
+        "Vidéos": [
+            "[Apprendre PHP - Grafikart](https://www.youtube.com/watch?v=cWoq5znh0vw&list=PLjwdMgw5TTLVDv-ceONHM_C19dPW1MAMD)",
+            "[Mise en pratique de la POO en PHP - Grafikart](https://www.youtube.com/watch?v=2UlpVWI0uFY&list=PLjwdMgw5TTLXP6JWACTxDqun0jJ5_sYvK)",
+        ]
+    },
+    "TypeScript": {
+        "Documentation": [
+            "[TypeScript documentation](https://www.typescriptlang.org/docs/)",
+        ],
+        "Vidéos": [
+            "[Apprendre TypeScript - Grafikart](https://www.youtube.com/watch?v=ffCIANfx_-0&list=PLjwdMgw5TTLX1tQ1qDNHTsy_lrkCt4VW3)",
+        ]
+    },
+    "Ruby": {
+        "Documentation": [
+            "[Ruby documentation](https://www.ruby-lang.org/fr/documentation/)",
+        ],
+        "Vidéos": [
+            "[Apprendre Ruby - Grafikart](https://www.youtube.com/watch?v=vgSQ97FDSvM&list=PLjwdMgw5TTLVVJHvstDYgqTCao-e-BgA8)",
+            "[Ruby on Rails (5.X) - Grafikart](https://www.youtube.com/watch?v=AG2mMAsuMwQ&list=PLjwdMgw5TTLWfI1B2Wv2WPgR9iOyw12zi)",
+        ]
+    },
+    "Python": {
+        "Documentation": [
+            "[Python documentation](https://docs.python.org/fr/3/)",
+        ],
+        "Vidéos": [
+            "[Python 3 pour débutants - GetCodingKnowledge](https://www.youtube.com/watch?v=ga5QRheqz2E&list=PLuWyq_EO5_AJRcJvqTphOv-OAqVXD2txS)",
+        ]
+    },
+    "React / React Native": {
+        "Documentation": [
+            "[React documentation](https://reactjs.org/docs/getting-started.html)",
+            "[React Native documentation](https://reactnative.dev/docs/getting-started)",
+        ],
+        "Vidéos": [
+            "[Apprendre React - Grafikart](https://www.youtube.com/watch?v=SMgQlTSoXf0&list=PLjwdMgw5TTLWom67YfZuha-1iYzIirwJR)",
+        ]
+    },
+    "VueJS": {
+        "Documentation": [
+            "[VueJS 3 documentation](https://vuejs.org/)",
+            "[VueJS 3 documentation](https://vuejs.org/guide/introduction.html)",
+            "[VueJS 2 documentation](https://v2.vuejs.org/)",
+            "[VueJS 2 guide](https://fr.vuejs.org/v2/guide/)",
+        ],
+        "Vidéos": [
+            "[VueJS 2 - Grafikart](https://www.youtube.com/watch?v=g7YKecZhFRA&list=PLjwdMgw5TTLW-mAtlR46VajrKs4dep3y0)",
+        ]
+    },    
+};
